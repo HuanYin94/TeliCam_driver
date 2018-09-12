06379b28adf3492740d099ddcd99ba8dd491a775
Huan Yin <zjuyinhuan@gmail.com>
2018-09-12 14:59:41 +0800

    no build

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yh/mine_ws/src/TeliCam_driver
-# Build directory: /home/yh/mine_ws/src/TeliCam_driver/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
